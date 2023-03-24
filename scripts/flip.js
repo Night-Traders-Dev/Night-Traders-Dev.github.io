67099fa905cec34446da43e44618816e505b506d
Night-Traders-Dev <quegmeister@gmail.com>
2023-03-24 00:59:40 +0000

    update

--- scripts/flip.js
+++ scripts/flip.js
@@ -1,6 +1,368 @@
-const contractAddress = "0x909dE4D8856d041335EaE89d421D331026C95e7d";
-const contractABI = [
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            await window.ethereum.enable();
+        } catch (error) {
+            console.error('User denied account access.');
+        }
+    } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+        console.error('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
+
+document.getElementById('approve').addEventListener('click', approveTokens);
+document.getElementById('play').addEventListener('click', playCoinFlip);
+
+async function approveTokens() {
+    const tokenContractABI = [
   {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      }
+    ],
+    "name": "allowance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "subtractedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "decreaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "addedValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "increaseAllowance",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+    const tokenContractAddress = '0x7def6e73B2Be4D31fe1c918c3b55907cFc21bA8D'; // Replace with the token contract address
+    const coinflipContractAddress = '0x909dE4D8856d041335EaE89d421D331026C95e7d'; // Replace with the coinflip contract address
+
+    const amountToApprove = document.getElementById('amount').value;
+    const amountInWei = web3.utils.toWei(amountToApprove, 'ether');
+
+    const accounts = await web3.eth.getAccounts();
+    const userAddress = accounts[0];
+
+    const tokenContract = new web3.eth.Contract(tokenContractABI, tokenContractAddress);
+    await tokenContract.methods.approve(coinflipContractAddress, amountInWei).send({ from: userAddress });
+
+    console.log('Tokens approved successfully.');
+}
+
+async function playCoinFlip() {
+    const coinflipContractABI = [ {
     "inputs": [],
     "stateMutability": "nonpayable",
     "type": "constructor"
@@ -481,73 +843,17 @@ const contractABI = [
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
-  }
-];
+  }];
+    const coinflipContractAddress = '0x909dE4D8856d041335EaE89d421D331026C95e7d'; // Replace with the coinflip contract address
 
-const polyPenTokenAddress = "0x7def6e73B2Be4D31fe1c918c3b55907cFc21bA8D";
+    const amountToPlay = document.getElementById('amount').value;
+    const amountInWei = web3.utils.toWei(amountToPlay, 'ether');
 
-window.addEventListener("load", async () => {
-  if (window.ethereum) {
-    window.web3 = new Web3(ethereum);
-    try {
-      await ethereum.enable();
-      initApp();
-    } catch (error) {
-      console.error("User denied account access.");
-    }
-  } else if (window.web3) {
-    window.web3 = new Web3(web3.currentProvider);
-    initApp();
-  } else {
-    console.error("No web3? You should consider trying MetaMask!");
-  }
-});
+    const accounts = await web3.eth.getAccounts();
+    const userAddress = accounts[0];
 
-function initApp() {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
-  const tokenInstance = new web3.eth.Contract(contractABI, polyPenTokenAddress);
+    const coinflipContract = new web3.eth.Contract(coinflipContractABI, coinflipContractAddress);
+    await coinflipContract.methods.coinflip(amountInWei).send({ from: userAddress });
 
-  document.getElementById("connect-wallet").addEventListener("click", async () => {
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    const account = accounts[0];
-    document.getElementById("game-controls").removeAttribute("hidden");
-  });
-
-  document.getElementById("approve-tokens").addEventListener("click", async () => {
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    const account = accounts[0];
-    const amountToApprove = web3.utils.toWei("1", "ether");
-
-    tokenInstance.methods
-      .approve(contractAddress, amountToApprove)
-      .send({ from: account })
-      .on("transactionHash", (hash) => {
-        console.log("Approval transaction submitted.");
-      })
-      .on("receipt", (receipt) => {
-        console.log("Approval transaction confirmed.");
-      });
-  });
-
-  document.getElementById("play-coinflip").addEventListener("click", async () => {
-      try {
-        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-        const account = accounts[0];
-        const betAmount = document.getElementById("bet-amount").value;
-
-        if (!betAmount || isNaN(betAmount) || Number(betAmount) <= 0) {
-          alert("Please enter a valid bet amount.");
-          return;
-        }
-
-        const weiBetAmount = web3.utils.toWei(betAmount, "ether");
-        await contractInstance.methods.coinflip(weiBetAmount).send({ from: account, value: web3.utils.toWei("0.1", "ether") });
-
-        console.log("Coin flip game played");
-      } catch (error) {
-        console.error("Error playing coin flip game:", error.message);
-      }
-    });
-
-  });
+    console.log('Coin flip played successfully.');
 }
