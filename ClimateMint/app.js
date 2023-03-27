b989f77f39a01d0813bb33a0c39a7ce65294f012
Night-Traders-Dev <quegmeister@gmail.com>
2023-03-27 22:46:34 +0000

    update

--- ClimateMint/app.js
+++ ClimateMint/app.js
@@ -545,7 +545,7 @@ const contractABI = [
     "type": "function"
   }
 ]; // Replace with the ABI of the PenthouseClimateModule contract
-const contractAddress = "0xbC0041a35C396E7A4637aC7896615f7795cC09bF";
+const contractAddress = "0x8c987637473f876cE2DeAb2CaA3f66D962D1B8C9";
 
 let web3;
 let contract;
