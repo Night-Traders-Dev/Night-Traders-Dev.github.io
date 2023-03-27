d39e196ccb5cd88b2ba49f753d0405b61337f99c
Night-Traders-Dev <quegmeister@gmail.com>
2023-03-27 19:48:50 +0000

    update

--- ClimateMint/app.js
+++ ClimateMint/app.js
@@ -13,6 +13,10 @@ async function connectMetamask() {
         try {
             const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
             userAddress = accounts[0];
+
+            // Update this line to use the Polygon network's RPC URL
+            web3 = new Web3("https://polygon-rpc.com");
+
             contract = new web3.eth.Contract(contractABI, contractAddress);
 
             document.getElementById("connectBtn").classList.add("hidden");
@@ -32,7 +36,7 @@ async function mintNFTs() {
     const price = await contract.methods.PRICE().call();
     const totalCost = price * numTokens;
 
-    contract.methods.mint(numTokens).send({ from: userAddress, value: totalCost })
+    contract.methods.mint(numTokens).send({ from: userAddress, value: totalCost, gas: 210000, gasPrice: 1e9 }) // Adjust the gas and gasPrice as needed
         .on("transactionHash", (hash) => {
             console.log("Transaction Hash:", hash);
         })
