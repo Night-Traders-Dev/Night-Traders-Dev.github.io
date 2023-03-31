ef2c9ddd355d5acf8b284cd77976815c29adc6db
Night-Traders-Dev <quegmeister@gmail.com>
2023-03-31 19:22:18 +0000

    update

--- ClimateMint/app.js
+++ ClimateMint/app.js
@@ -1,6 +1,6 @@
 // main.js
 
-import { connectWallet } from '../scripts/connectWallet.js';
+import { connectWallet } from './connectWallet.js';
 
 // Call the connectWallet() function at the appropriate point in your application flow
 connectWallet();
